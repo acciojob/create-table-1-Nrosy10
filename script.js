d335da8f7b0ca79af84a173de63853682c08f1a6
Nrosy10 <117885685+Nrosy10@users.noreply.github.com>
2023-06-23 10:04:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function insert_Row() {
     //Write your code here
-  
+  var table = document.getElementById("sampleTable");
+  var newRow = table.insertRow(0); // Insert row at index 1 (below the header row)
+
+  var cell1 = newRow.insertCell(0); // Insert cell at index 0
+  var cell2 = newRow.insertCell(1); // Insert cell at index 1
+
+  cell1.innerHTML = "New Cell1";
+  cell2.innerHTML = "New Cell2";
   
 }
